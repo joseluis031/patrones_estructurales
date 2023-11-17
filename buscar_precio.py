790d6f00828d8cc6118894fef52c94ad36dd9d3c
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-11-17 12:02:03 +0100

    consigo precio de cada cosa por terminal

--- buscar_precio.py
+++ buscar_precio.py
@@ -7,7 +7,7 @@ def buscar_preciopizza(pizza):
                 if pizza in row:
                     return f"El precio de {pizza} es {row[1]}"
                 
-a = buscar_preciopizza("margarita")
+a = buscar_preciopizza("a")
 print(a)      
 
 def buscar_precioentrante(entrante):
@@ -16,7 +16,7 @@ def buscar_precioentrante(entrante):
         for row in reader:
             if entrante in row[9]:
                 return f"El precio de {entrante} es {row[10]}" 
-b = buscar_precioentrante("calamares")
+b = buscar_precioentrante("aa")
 print(b)     
 
 def buscar_preciobebida(bebida):
@@ -26,7 +26,7 @@ def buscar_preciobebida(bebida):
             if bebida in row[3]:
                 return f"El precio de {bebida} es {row[4]}"
             
-c = buscar_preciobebida("coca-cola")
+c = buscar_preciobebida("aa")
 print(c)
 
 def buscar_preciopostre(postre):
@@ -36,6 +36,6 @@ def buscar_preciopostre(postre):
             if postre in row[6]:
                 return f"El precio de {postre} es {row[7]}"
             
-d = buscar_preciopostre("tarta de queso")
+d = buscar_preciopostre("a")
 print(d)
 
--- composite_menu.py
+++ composite_menu.py
@@ -25,58 +25,61 @@ class Component_menu(ABC):
     def operation(self) -> str:
         pass
     
-    @abstractmethod
-    def precio(self) -> float:
-        pass
+
 
 
 class Leaf_pizza(Component_menu):
     
-    def __init__(self, pizza: str, precio: float) -> None:
+    def __init__(self, pizza) -> None:
         self._pizza = pizza
-        self._precio = precio
         
-    def buscar_preciopizza(self, pizza):
-        with open('precio_elemento.csv', newline='') as File:  
+    def operation(self):
+        with open('precio_elementos.csv', newline='') as File:
             reader = csv.reader(File)
             for row in reader:
-                if pizza in row:
-                    return row[1]
+                if self._pizza in row:
+                    return f"El precio de {self._pizza} es {row[1]}"
 
-    def operation(self) -> str:
-        return f"{self._pizza} cuesta:  {self._precio}€"
 
     
 class Leaf_bebida(Component_menu):
 
-    def __init__(self, bebida: str, precio: float) -> None:
+    def __init__(self, bebida) -> None:
         self._bebida = bebida
-        self._precio = precio
     
     
-    def operation(self) -> str:
-        return f"{self._bebida} cuesta:  {self._precio}€"
+    def operation(self):
+        with open('precio_elementos.csv', newline='') as File:
+            reader = csv.reader(File)
+            for row in reader:
+                if self._bebida in row[3]:
+                    return f"El precio de {self._bebida} es {row[4]}"
     
 class Leaf_entrante(Component_menu):
 
-    def __init__(self, entrante: str, precio: float) -> None:
+    def __init__(self, entrante) -> None:
         self._entrante = entrante
-        self._precio = precio
         
 
-    def operation(self) -> str:
-        return f"{self._entrante} cuesta:  {self._precio}€"
-    
+    def operation(self):
+        with open('precio_elementos.csv', newline='') as File:
+            reader = csv.reader(File)
+            for row in reader:
+                if self._entrante in row[9]:
+                    return f"El precio de {self._entrante} es {row[10]}" 
     
 class Leaf_postre(Component_menu):
 
-    def __init__(self, postre: str, precio: float) -> None:
+    def __init__(self, postre) -> None:
         self._postre = postre
-        self._precio = precio
         
 
-    def operation(self) -> str:
-        return f"{self._postre} cuesta:  {self._precio}€"
+    def operation(self):
+        with open('precio_elementos.csv', newline='') as File:
+            reader = csv.reader(File)
+            for row in reader:
+                if self._postre in row[6]:
+                    return f"El precio de {self._postre} es {row[7]}"
 
 
 class Composite_combo1(Component_menu):
@@ -134,4 +137,57 @@ def client_code2(component1: Component_menu, component2: Component_menu) -> None
 if __name__ == "__main__":
     
     
+    bienvenida = input("¿quieres un menu? (si/no) ")
     
+    if bienvenida == "si":
+        print("Vamos a ello!!")
+        pizza = input("¿que pizza quieres? Tenemos:  margarita, barbacoa, 4 quesos, carbonara, hawaiana, romana, 4 estaciones, vegetal, napolitana o a :")
+        lista_pizza = [ "margarita", "barbacoa", "4 quesos", "carbonara", "hawaiana", "romana", "4 estaciones", "vegetal", "napolitana", "a"]
+        while pizza not in lista_pizza:
+            pizza = input("No tenemos esa, ¿que pizza quieres? ")
+            
+        bebida = input("¿que bebida quieres? Tenemos: coca-cola, fanta, agua o aa :")
+        lista_bebida = ["coca-cola", "fanta", "agua", "aa"]
+        while bebida not in lista_bebida:
+            bebida = input("No tenemos esa, ¿que bebida quieres? ")
+            
+        entrante = input("¿que entrante quieres? Tenemos: patatas, calamares, ensalada o aa :")
+        lista_entrante = ["patatas", "calamares", "ensalada", "aa"]
+        while entrante not in lista_entrante:
+            entrante = input("No tenemos esa, ¿que entrante quieres? ")
+            
+        postre = input("¿que postre quieres? Tenemos: tarta de queso, tarta de chocolate, helado o a :")
+        lista_postre = ["tarta de queso", "tarta de chocolate", "helado", "a"]
+        while postre not in lista_postre:
+            postre = input("No tenemos esa, ¿que postre quieres? ")
+        
+        print ("Tu pedido es: ", pizza, bebida, entrante, postre, " y el precio total es: ")
+            
+        
+        
+        
+        
+            
+        pedido1_pizza = Leaf_pizza(pizza)
+        print(pedido1_pizza.operation())
+        
+        pedido1_bebida = Leaf_bebida(bebida)
+        print(pedido1_bebida.operation())
+        
+        pedido1_entrante = Leaf_entrante(entrante)
+        print(pedido1_entrante.operation())
+        
+        pedido1_postre = Leaf_postre(postre)
+        print(pedido1_postre.operation())
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    else:
+        print("hasta otra")
+        exit()

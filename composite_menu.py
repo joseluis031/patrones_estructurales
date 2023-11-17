e8b5042f5a4925e53c1eb76540a55d7eb20ea978
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-11-17 17:30:13 +0100

    creo los combos temporales

--- composite_menu.py
+++ composite_menu.py
@@ -165,7 +165,7 @@ def client_code2(component1: Component_menu, component2: Component_menu) -> None
 
 if __name__ == "__main__":
     
-    bienvenida = input("¿Que deseas(1,2,3 o 4): 1. Crear tu propio menu, 2. Elegir un menu ya creado, 3. Un producto suelto o 4. Nada ")
+    bienvenida = input("¿Que deseas(1,2,3,4 0 5): 1. Crear tu propio menu, 2. Elegir un menu ya creado, 3. Un producto suelto, 4. Un Combo o 5. Nada ")
     
     
     if bienvenida == "1":
@@ -329,7 +329,142 @@ if __name__ == "__main__":
         
         
         
-    
+    elif bienvenida == "4":
+        print("Tenemos los siguientes combos:")
+        print("1. Combo Pareja: 2 pizzas: margarita y 4 quesos, 2 refrescos, aceitunas y croquetas  y 2 helados")
+        print("2. Combo Familiar: 2 pizzas margarita y 2 barbacoa, 2 aguas y 2 refrescos, 2 alitas y 2 de patatas y 4 tartas de queso")
+        print("3. Combo Maxi muerte por chocolat: 6 pizzas barbacoa, 6 agua, 6 alitas de pollo y 6 tarta de chocolate")
+        combo = input("Elige el numero del combo que quieres: ")
+        lista_combo = ["1", "2", "3"]
+        while combo not in lista_combo:
+            combo = input("No tenemos esa, ¿que combo quieres? ")
+            
+        if combo == "1":
+            combo1 = Composite_combo1("Combo Pareja")
+            pedido1_pizza = Leaf_pizza("margarita")
+            pedido1_pizza2 = Leaf_pizza("4 quesos")
+            pedido1_bebida = Leaf_bebida("fanta")
+            pedido1_bebida2 = Leaf_bebida("coca-cola")
+            pedido1_entrante = Leaf_entrante("aceitunas")
+            pedido1_entrante2 = Leaf_entrante("croquetas")
+            pedido1_postre = Leaf_postre("helado")
+            pedido1_postre2 = Leaf_postre("helado")
+            
+            
+            combo1.add(pedido1_pizza)
+            combo1.add(pedido1_pizza2)
+
+            combo1.add(pedido1_bebida)
+            combo1.add(pedido1_bebida2)
+            combo1.add(pedido1_entrante)
+            combo1.add(pedido1_entrante2)
+            
+            combo1.add(pedido1_postre)
+            combo1.add(pedido1_postre2)
+            
+            print(f"El precio total del combo pareja es: {combo1.precio()}")
+            
+        elif combo == "2":
+            combo1 = Composite_combo1("Combo Familiar")
+            pedido1_pizza = Leaf_pizza("margarita")
+            pedido1_pizza2 = Leaf_pizza("barbacoa")
+            pedido1_pizza3 = Leaf_pizza("margarita")
+            pedido1_pizza4   = Leaf_pizza("barbacoa")
+            pedido1_bebida = Leaf_bebida("agua")
+            pedido1_bebida2 = Leaf_bebida("agua")
+            pedido1_bebida3 = Leaf_bebida("coca-cola")
+            pedido1_bebida4 = Leaf_bebida("coca-cola")
+            
+            pedido1_entrante = Leaf_entrante("alitas de pollo")
+            pedido1_entrante2 = Leaf_entrante("alitas de pollo")
+            pedido1_entrante3 = Leaf_entrante("patatas")
+            pedido1_entrante4 = Leaf_entrante("patatas")
+            
+            
+            pedido1_postre = Leaf_postre("tarta de queso")
+            pedido1_postre2 = Leaf_postre("tarta de queso")
+            pedido1_postre3 = Leaf_postre("tarta de queso")
+            pedido1_postre4 = Leaf_postre("tarta de queso")
+            
+            
+            combo1.add(pedido1_pizza)
+            combo1.add(pedido1_pizza2)
+            combo1.add(pedido1_pizza3)
+            combo1.add(pedido1_pizza4)
+            combo1.add(pedido1_bebida)
+            combo1.add(pedido1_bebida2)
+            combo1.add(pedido1_bebida3)
+            combo1.add(pedido1_bebida4)
+            combo1.add(pedido1_entrante)
+            combo1.add(pedido1_entrante2)
+            combo1.add(pedido1_entrante3)
+            combo1.add(pedido1_entrante4)
+            
+            combo1.add(pedido1_postre)
+            combo1.add(pedido1_postre2)
+            combo1.add(pedido1_postre3)
+            combo1.add(pedido1_postre4)
+            
+            print(f"El precio total del combo familiar es: {combo1.precio()}")
+            
+        elif combo == "3":
+            combo1 = Composite_combo1("Combo Maxi")
+            pedido1_pizza = Leaf_pizza("barbacoa")
+            pedido1_pizza2 = Leaf_pizza("barbacoa")
+            pedido1_pizza3 = Leaf_pizza("barbacoa")
+            pedido1_pizza4   = Leaf_pizza("barbacoa")
+            pedido1_pizza5 = Leaf_pizza("barbacoa")
+            pedido1_pizza6 = Leaf_pizza("barbacoa")
+            pedido1_bebida = Leaf_bebida("agua")
+            pedido1_bebida2 = Leaf_bebida("agua")
+            pedido1_bebida3 = Leaf_bebida("agua")
+            pedido1_bebida4 = Leaf_bebida("agua")
+            pedido1_bebida5 = Leaf_bebida("agua")
+            pedido1_bebida6 = Leaf_bebida("agua")
+            
+            pedido1_entrante = Leaf_entrante("alitas de pollo")
+            pedido1_entrante2 = Leaf_entrante("alitas de pollo")
+            pedido1_entrante3 = Leaf_entrante("alitas de pollo")
+            pedido1_entrante4 = Leaf_entrante("alitas de pollo")
+            pedido1_entrante5 = Leaf_entrante("alitas de pollo")
+            pedido1_entrante6 = Leaf_entrante("alitas de pollo")
+            
+            pedido1_postre = Leaf_postre("tarta de chocolate")
+            pedido1_postre2 = Leaf_postre("tarta de chocolate")
+            pedido1_postre3 = Leaf_postre("tarta de chocolate")
+            pedido1_postre4 = Leaf_postre("tarta de chocolate")
+            pedido1_postre5 = Leaf_postre("tarta de chocolate")
+            pedido1_postre6 = Leaf_postre("tarta de chocolate")
+            
+            combo1.add(pedido1_pizza)
+            combo1.add(pedido1_pizza2)
+            combo1.add(pedido1_pizza3)
+            combo1.add(pedido1_pizza4)
+            combo1.add(pedido1_pizza5)
+            combo1.add(pedido1_pizza6)
+            combo1.add(pedido1_bebida)
+            combo1.add(pedido1_bebida2)
+            combo1.add(pedido1_bebida3)
+            combo1.add(pedido1_bebida4)
+            combo1.add(pedido1_bebida5)
+            combo1.add(pedido1_bebida6)
+            combo1.add(pedido1_entrante)
+            combo1.add(pedido1_entrante2)
+            combo1.add(pedido1_entrante3)
+            combo1.add(pedido1_entrante4)
+            combo1.add(pedido1_entrante5)
+            combo1.add(pedido1_entrante6)
+            combo1.add(pedido1_postre)
+            combo1.add(pedido1_postre2)
+            combo1.add(pedido1_postre3)
+            combo1.add(pedido1_postre4)
+            combo1.add(pedido1_postre5)
+            combo1.add(pedido1_postre6)
+            
+            
+            print(f"El precio total del combo maxi es: {combo1.precio()}")
+            
+        
          
     else:
         print("Vale, adios")

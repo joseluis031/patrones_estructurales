9b5b1e660f6f3dcc7ba105ed5125c802298b2399
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-11-26 13:55:06 +0100

    funciones para json

--- Ejercicio Proxi/proxi.py
+++ Ejercicio Proxi/proxi.py
@@ -12,12 +12,15 @@ class Component(ABC):
     @abstractmethod
     def operation(self) -> None:
         pass
+    
+    def to_dict(self):
+        raise NotImplementedError("to_dict method must be implemented in subclasses")
 
 
 class Carpeta(Component):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, nombre):
+        self.nombre = nombre
         self._children = []
     
     def add(self, component: Component) -> None:
@@ -27,10 +30,17 @@ class Carpeta(Component):
         self._children.remove(component)
     
     def operation(self) -> str:
-        results = [f"Composite: {self.name}"]
+        results = [f"Composite: {self.nombre}"]
         for child in self._children:
             results.append(child.operation())
         return '\n'.join(results)
+    
+    def to_dict(self):
+        return {
+            "tipo": "Carpeta",
+            "nombre": self.nombre,
+            "children": [child.to_dict() for child in self._children]
+        }
             
         
 class Documentos_Leaf(Component):
@@ -38,9 +48,19 @@ class Documentos_Leaf(Component):
         self.nombre = nombre
         self.tipo_documento = tipo_documento
         self.tamaño = tamaño
+        self.access_log = []
         
     def operation(self):
         return  f"Leaf: {self.nombre} ({self.tipo_documento}, {self.tamaño} KB)"
+    
+    def to_dict(self):
+        return {
+            "tipo": "Documento",
+            "nombre": self.nombre,
+            "tipo_documento": self.tipo_documento,
+            "tamaño": self.tamaño,
+            "access_log": self.access_log
+        }
 
 class Enlace_Leaf(Component):
     def __init__(self, nombre, link):
@@ -49,7 +69,13 @@ class Enlace_Leaf(Component):
         
     def operation(self):
         return f"Enlace: {self.nombre} -> {self.link}"
-
+    
+    def to_dict(self):
+        return {
+            "tipo": "Enlace",
+            "nombre": self.nombre,
+            "link": self.link
+        }
 
 class Proxy(Component):
     """
@@ -58,18 +84,30 @@ class Proxy(Component):
 
     def __init__(self, real_subject):
         self.real_subject = real_subject
-        self.access_log = {}
+        self.access_log = []
 
     def operation(self):
         document_name = self.real_subject.nombre
         if self.check_access(document_name):
             self.real_subject.operation()
-            self.access_log[document_name] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.access_log(document_name)
        
-    def check_access(self) -> bool:
+    def check_access(self, document_name) -> bool:
         print("Proxy: Checking access prior to firing a real request.")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.access_log.append({'document_name': document_name , 'timestamp': timestamp})
         return True
 
+
+    def access_log(self) -> None:
+        print("Proxy: Logging the time of request.", end="")
+
+    def to_dict(self):
+        return {
+            'type': 'Proxy',
+            'real_subject': self.real_subject.to_dict(),
+            'access_log': self.access_log
+        }
 
 
 

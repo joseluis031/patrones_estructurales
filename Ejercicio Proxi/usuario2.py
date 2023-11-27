114dec6987aec20e937563217055201f3ff0ff33
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-11-27 11:02:20 +0100

    funcion para eliminar

--- Ejercicio Proxi/usuario2.py
+++ Ejercicio Proxi/usuario2.py
@@ -85,7 +85,8 @@ def realizar_operacion(usuario_actual, carpeta_principal):
         print(f"Carpeta '{nombre_carpeta}' añadida correctamente.")
     elif opcion == "6":
         # Editar documentos, enlaces o carpetas existentes
-        agregar_elemento_a_carpeta(carpeta_principal)
+        borrar_elemento(carpeta_principal)
+
     else:
         print("Opción no válida.")
 
@@ -190,6 +191,62 @@ def agregar_elemento_a_carpeta(carpeta_principal):
         print(f"Elemento '{nombre_elemento}' añadido correctamente a la carpeta '{carpeta_seleccionada.nombre}'.")
     except (ValueError, IndexError):
         print("Selección no válida.")
+
+
+
+
+
+def borrar_elemento(carpeta):
+    # Inputs
+    tipo_elemento = input("Ingrese el tipo del elemento (Carpeta/Documento/Enlace): ")
+    nombre_elemento = input("Ingrese el nombre del elemento que desea borrar: ")
+
+    # Verificar si la carpeta contiene elementos
+    if not carpeta._children:
+        print(f"No hay elementos para borrar en la carpeta '{carpeta.nombre}'.")
+        return
+
+    # Buscar el elemento por tipo y nombre
+    elemento_encontrado = None
+    for elemento in carpeta._children:
+        if isinstance(elemento, Carpeta) and elemento.nombre == nombre_elemento and tipo_elemento == "Carpeta":
+            elemento_encontrado = elemento
+            break
+        elif isinstance(elemento, Documentos_Leaf) and elemento.nombre == nombre_elemento and tipo_elemento == "Documento":
+            elemento_encontrado = elemento
+            break
+        elif isinstance(elemento, Enlace_Leaf) and elemento.nombre == nombre_elemento and tipo_elemento == "Enlace":
+            elemento_encontrado = elemento
+            break
+
+    # Si se encuentra el elemento, eliminarlo y actualizar el JSON
+    if elemento_encontrado:
+        carpeta.remove(elemento_encontrado)
+        guardar_estructura_en_json(carpeta, "Ejercicio Proxi/basedatos.json")
+        print(f"Elemento '{nombre_elemento}' ({tipo_elemento}) eliminado correctamente de la carpeta '{carpeta.nombre}'.")
+    else:
+        print(f"No se encontró el elemento '{nombre_elemento}' ({tipo_elemento}) en la carpeta '{carpeta.nombre}'.")
+        
+
+# Llamada a la función
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

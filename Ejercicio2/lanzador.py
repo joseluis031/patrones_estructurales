acd8ce4f42a9d8aa62b1921b4c3ed91958c08e2c
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-11-28 11:44:48 +0100

    borro

--- Ejercicio2/lanzador.py
+++
@@ -1,6 +0,0 @@
-from interfaz import *
-
-def lanzador():
-    pedido_anterior = input("¿Has realizado un pedido anteriormente? (si/no): ")
-
-    interfaz(pedido_anterior)
--- Ejercicio2/main.py
+++
@@ -1,4 +0,0 @@
-from lanzador import *
-
-if __name__ == "__main__":
-    lanzador()
